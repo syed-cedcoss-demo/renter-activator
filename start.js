92fdff9a1ecb99205b827f8a2ef87e43bd187c52
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-03-23 17:04:21 +0530

    added more url

--- start.js
+++ start.js
@@ -2,14 +2,33 @@ import axios from "axios";
 const main = () => {
   const zohoImportUploadApp = async () => {
     try {
-      const res = await axios.get("https://zoho-product-upload.onrender.com/");
+      const res = await axios.get("https://zoho-product-upload.onrender.com");
+      console.log("res?.data", res?.data);
+    } catch (error) {
+      console.log("error in zoho upload api");
+    }
+  };
+  const zohoDesignApp = async () => {
+    try {
+      const res = await axios.get("https://zoho-new-design.onrender.com");
+      console.log("res?.data", res?.data);
+    } catch (error) {
+      console.log("error in zoho upload api");
+    }
+  };
+  const nodeSetupApp = async () => {
+    try {
+      const res = await axios.get("https://node-setup-server.onrender.com/");
       console.log("res?.data", res?.data);
     } catch (error) {
       console.log("error in zoho upload api");
     }
   };
   setInterval(() => {
+    console.log("cron job trigger");
     zohoImportUploadApp();
+    zohoDesignApp();
+    nodeSetupApp();
   }, 1000 * 60 * 10);
 };
 main();
