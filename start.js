2039cdb4fe52d18ff92cc5ce3db423bf72045712
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-03-27 14:26:59 +0530

    code chages

--- start.js
+++ start.js
@@ -1,11 +1,11 @@
 import axios from "axios";
+import express from "express";
+const app = express();
 const main = () => {
   const zohoImportUploadApp = async () => {
     try {
       await axios.get("https://zoho-product-upload.onrender.com");
-      helper();
     } catch (error) {
-      helper();
       console.log("error in zoho upload api");
     }
   };
@@ -23,14 +23,16 @@ const main = () => {
       console.log("error in zoho upload api");
     }
   };
-  const helper = () => {
-    setTimeout(() => {
-      console.log("cron job trigger");
-      zohoImportUploadApp();
-      zohoDesignApp();
-      nodeSetupApp();
-    }, 1000 * 60 * 5);
-  };
-  helper();
+  setInterval(() => {
+    console.log("cron job trigger");
+    zohoImportUploadApp();
+    zohoDesignApp();
+    nodeSetupApp();
+  }, 1000 * 60 * 5);
 };
-main();
+
+app.get("/start", (req, res) => {
+  main();
+  res.status(200).send("interval start");
+});
+app.listen(3002, () => console.log("server started"));
